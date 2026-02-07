3c563cd9bc51254973bf4c9fd67be0a9a24cfa9c
Anusha S18l <3714727+anushas@users.noreply.github.com>
2026-02-06 20:16:28 -0800

    comment

--- src/yb/consensus/consensus_peers.cc
+++ src/yb/consensus/consensus_peers.cc
@@ -595,6 +595,8 @@ void Peer::ProcessRemoteBootstrapResponse() {
   if (!status.ok()) {
     YB_LOG_WITH_PREFIX_EVERY_N_SECS(WARNING, 30)
         << "Unable to begin remote bootstrap on peer: " << status;
+    // TODO: for failure due to checkpoint lock contention,
+    // should we delay the retry.
     return;
   }
 
